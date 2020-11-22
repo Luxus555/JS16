6c33f9908d8c47b4b9013ff2f61631270430077a
Luxus555 <luxus555@gmail.com>
2020-11-22 14:59:36 +0000

    Начало урока 2.1

--- script/script.js
+++ script/script.js
@@ -5,5 +5,14 @@ let deposit = true;
 let mission = 10000;
 let period = 12;
 
-alert("Любой текст!");
-console.log("И тут любой текст");
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log("Период равен " + period + " месяцев.");
+console.log("Цель - заработать: " + mission + " фунтов");
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(" "));
+
+let budgetDay = money / 30;
+console.log(budgetDay);
