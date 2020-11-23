8d73a22335c2d99a1ca6c00aa1d3d9e96522f8f1
Luxus555 <luxus555@gmail.com>
2020-11-23 20:05:31 +0000

    Урок 3

--- script/script.js
+++ script/script.js
@@ -1,21 +1,54 @@
-let money = 2000;
-let income = "Frilance";
-let addExpenses = "Internet, House, Electricity";
-let deposit = true;
-let mission = 10000;
-let period = 12;
+"use strict";
 
+let money = prompt("Ваш месячный доход");
 console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
+let money1 = prompt("Ваш месячный доход");
+console.log(typeof money1);
+
+let addExpenses = prompt(
+  "Перечислите возможные расходы за рассчитываемый период через запятую",
+  ""
+);
 console.log(addExpenses.length);
+let addExpenses1 = prompt(
+  "Перечислите возможные расходы за рассчитываемый период через запятую",
+  ""
+);
+
+let deposit = confirm("Есть ли у вас депозит в банке?");
+console.log(typeof deposit);
+let deposit1 = confirm("Есть ли у вас депозит в банке?");
+
+let expenses = prompt("Общие расходы");
+let expenses2 = prompt("Общие расходы");
+
+// let income = "Frilance";
+// console.log(typeof income);
+
+let budgetMonth = money - expenses;
+console.log("Бюджет на месяц " + budgetMonth);
+
+let mission = 100000;
+console.log("Цель заработать " + mission + " фунтов");
+let period = Math.ceil(mission / budgetMonth);
+console.log("Период равен " + period + " месяцев");
+console.log("Цель будет достигнута за: " + period + " месяцев");
+
 console.log("Период равен " + period + " месяцев.");
 console.log("Цель - заработать: " + mission + " фунтов");
 console.log(addExpenses.toLowerCase());
 console.log(addExpenses.split(" "));
 
-let budgetDay = money / 30;
-console.log(budgetDay);
+let budgetDay = Math.floor(+money / 30);
+console.log("Бюджет на день: " + budgetDay);
+
+if (budgetDay > 1200) {
+  console.log("У вас высокий уровень дохода");
+} else if (budgetDay >= 600 && budgetDay <= 1200) {
+  console.log("У вас средний уровень дохода");
+} else if (budgetDay < 600 && budgetDay > 0) {
+  console.log("К сожалению у вас уровень дохода ниже среднего");
+} else console.log("Что то пошло не так");
 
 let num = 266219,
   result = [],
