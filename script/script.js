050c4b099ea30ecf8e914c0ebf453eab44e3ffd1
Luxus555 <luxus555@gmail.com>
2020-11-24 16:14:24 +0000

    Исправление 3 урока

--- script/script.js
+++ script/script.js
@@ -1,41 +1,32 @@
 "use strict";
 
-let money = prompt("Ваш месячный доход");
+let money = +prompt("Ваш месячный доход?"),
+  income = "Freelance",
+  addExpenses = prompt(
+    "Перечислите возможные расходы за рассчитываемый период через запятую",
+    ""
+  ),
+  deposit = confirm("Есть ли у вас депозит в банке?"),
+  mission = 10000;
+
 console.log(typeof money);
-let money1 = prompt("Ваш месячный доход");
-console.log(typeof money1);
-
-let addExpenses = prompt(
-  "Перечислите возможные расходы за рассчитываемый период через запятую",
-  ""
-);
-console.log(addExpenses.length);
-let addExpenses1 = prompt(
-  "Перечислите возможные расходы за рассчитываемый период через запятую",
-  ""
-);
-
-let deposit = confirm("Есть ли у вас депозит в банке?");
+console.log(typeof income);
 console.log(typeof deposit);
-let deposit1 = confirm("Есть ли у вас депозит в банке?");
+console.log(addExpenses.length);
 
-let expenses = prompt("Общие расходы");
-let expenses2 = prompt("Общие расходы");
+let expenses1 = prompt("Введите обязательную статью расходов?"),
+  amount1 = +prompt("Во сколько это обойдется?"),
+  expenses2 = prompt("Введите обязательную статью расходов?"),
+  amount2 = +prompt("Во сколько это обойдется?");
 
-// let income = "Frilance";
-// console.log(typeof income);
-
-let budgetMonth = money - expenses;
+let budgetMonth = money - amount1 - amount2;
 console.log("Бюджет на месяц " + budgetMonth);
 
-let mission = 100000;
 console.log("Цель заработать " + mission + " фунтов");
 let period = Math.ceil(mission / budgetMonth);
 console.log("Период равен " + period + " месяцев");
 console.log("Цель будет достигнута за: " + period + " месяцев");
 
-console.log("Период равен " + period + " месяцев.");
-console.log("Цель - заработать: " + mission + " фунтов");
 console.log(addExpenses.toLowerCase());
 console.log(addExpenses.split(" "));
 
@@ -50,22 +41,15 @@ if (budgetDay > 1200) {
   console.log("К сожалению у вас уровень дохода ниже среднего");
 } else console.log("Что то пошло не так");
 
-let num = 266219,
+/*let num = 266219,
   result = [],
   newNumber = num.toString();
-
 for (let i = 0, j = newNumber.length; i < j; i += 1) {
   result.push(+newNumber[i]);
 }
-
 let proizvedenie = result.reduce((a, b) => a * b);
-
 console.log(proizvedenie);
-
 let stepen = proizvedenie ** 3;
-
 console.log(stepen);
-
 let Itogo = parseInt(stepen.toString().slice(0, 2));
-
-alert(Itogo);
+alert(Itogo);*/
