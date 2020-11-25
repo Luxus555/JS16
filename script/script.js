68cf7b553fdd71d2ca95fed7b87f313d3563de93
Luxus555 <luxus555@gmail.com>
2020-11-25 17:06:34 +0000

    4 урок почти готов

--- script/script.js
+++ script/script.js
@@ -4,7 +4,7 @@ let money = 0,
   deposit = true,
   mission = 100000,
   period = "12",
-  expenses1 = "",
+  //expenses1 = "",
   amount1 = 0,
   expenses2 = "",
   amount2 = 0,
@@ -19,18 +19,41 @@ addExpenses = prompt(
 );
 deposit = confirm("Есть ли у вас депозит в банке?", false);
 
-expenses1 = prompt("Введите обязательную статью расходов", "Детский сад");
+let expenses1 = prompt("Введите обязательную статью расходов", "Детский сад");
 amount1 = +prompt("Во сколько это обойдется", 1000);
 expenses1 = prompt("Введите обязательную статью расходов", "Ипотека");
 amount1 = +prompt("Во сколько это обойдется", 1000);
 
-budgetMonth = money - amount1 - amount2;
+let showTypeOf = function (item) {
+  console.log(typeof item);
+};
 
-budgetDay = Math.floor(budgetMonth / 30);
+showTypeOf(money);
+showTypeOf(income);
+showTypeOf(deposit);
 
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
+console.log(addExpenses.toLowerCase().split(","));
+
+function getExpensesMonth(amount1, amount2) {
+  return amount1 + amount2;
+}
+console.log("Расходы за месяц: " + getExpensesMonth());
+
+let getAccumulatedMonth = function () {
+  return money - getExpensesMonth();
+};
+
+let accumulatedMonth = getAccumulatedMonth();
+
+function getTargetMonth() {
+  return mission / accumulatedMonth;
+  console.log("Цель будет достигнута за: " + richTarget + " месяцев");
+}
+
+//budgetMonth = money - amount1 - amount2;
+
+budgetDay = Math.floor(accumulatedMonth / 30);
+
 console.log(addExpenses.length);
 console.log("Период равен " + period + " месяцев");
 console.log("Цель заработать " + mission + " фунтов");
@@ -48,22 +71,3 @@ if (budgetDay > 1200) {
 } else if (budgetDay < 600 && budgetDay > 0) {
   console.log("К сожалению у вас уровень дохода ниже среднего");
 } else console.log("Что то пошло не так");
-
-function getExpensesMonth(amount1, amount2) {
-  return amount1 + amount2;
-}
-getExpensesMonth(amount1, amount2);
-
-function getAccumulatedMonth() {
-  return console.log(money - amount1 - amount2);
-}
-getAccumulatedMonth();
-
-let accumulatedMonth = function getAccumulatedMonth() {
-  console.log(accumulatedMonth);
-};
-
-function getTargetMonth() {
-  console.log("Цель будет достигнута за: " + richTarget + " месяцев");
-}
-getTargetMonth();
