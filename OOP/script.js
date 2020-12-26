aeb0989b77c5765f366927066af91acb623b1ddc
Luxus555 <luxus555@gmail.com>
2020-12-26 19:27:24 +0000

    Обе части готовы

--- OOP/script.js
+++ OOP/script.js
@@ -1,11 +1,33 @@
 "use strict";
 
 class DomElement {
-  constructor(selector = 0, height = 0, width = 0, bg = 0, fontSize = 0) {
+  constructor(selector, height, width, bg, fontSize) {
     this.selector = selector;
     this.height = height;
     this.width = width;
     this.bg = bg;
     this.fontSize = fontSize;
   }
+  newElem() {
+    if (this.selector[0] === ".") {
+      let newDiv = document.createElement("div");
+      newDiv.classList.add(this.selector);
+      newDiv.style.cssText =
+        "height: $(this.height); width: $(this.width); background: $(this.bg); font-size: $(this.fontSize);";
+      newDiv.innerHTML = "<h2>Merry Christmas!</h2>";
+      document.body.append(newDiv);
+    } else if (this.selector[0] === "#") {
+      let newP = document.createElement("p");
+      newP.id = this.selector;
+      newP.style.cssText =
+        "height: $(this.height); width: $(this.width); background: $(this.bg); font-size: $(this.fontSize);";
+      newP.innerHTML = "<p>Я новый параграф!</p>";
+      document.body.append(newP);
+    }
+  }
 }
+const test = new DomElement(".block", "200px", "200px", "white", "16px");
+test.newElem();
+
+const test2 = new DomElement("#block", "250px", "250px", "white", "20px");
+test2.newElem();
